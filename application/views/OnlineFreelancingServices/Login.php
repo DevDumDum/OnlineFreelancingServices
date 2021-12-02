14cbdc2e1de9ef0511429b6505ff42823f69a81f
Franco de Guzman <tangkekofranco06@gmail.com>
2021-12-02 14:06:09 +0800

    REMOVED TITLE ON navbar.php

--- application/views/OnlineFreelancingServices/Login.php
+++ application/views/OnlineFreelancingServices/Login.php
@@ -1,4 +1,15 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type ="text/css" href="<?php echo base_url();?>public/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="<?php echo base_url();?>public/css/register.css"/>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<title>Franco Part</title>
 
 <body>
 	<p class="h1">User login</p>
--- application/views/OnlineFreelancingServices/inc/navbar.php
+++ application/views/OnlineFreelancingServices/inc/navbar.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="<?php echo base_url();?>public/css/register.css"/>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>GOMEZ-PERPETUA</title>
     <style>
         body{
             width: 100%;
