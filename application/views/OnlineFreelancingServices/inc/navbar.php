d30ec9027c72710808c0f46bdf478031ccd376f3
Franco de Guzman <tangkekofranco06@gmail.com>
2021-12-03 00:02:13 +0800

    navbar update

---
+++ application/views/OnlineFreelancingServices/inc/navbar.php
@@ -0,0 +1,35 @@
+
+        <nav class="navbar custom-navbar navbar-expand-md navbar-dark">
+            <div class="container-fluid ">
+                <a class="navbar-brand" href="#">
+                <img class="img-fluid" src="<?php echo base_url();?>public/images/logo.png" alt="" width="120" class="d-inline-block align-text-top">
+                </a>
+                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <ul class="nav justify-content-left collapse navbar-collapse" id="navbarCollapse">
+                    <li class="nav-item">
+                        <a href="index.html" class="nav-item active nav-link">Home</a></li>
+                    </li>
+                    <li class="nav-item">
+                        <a href="about.html" class="nav-item nav-link">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="skills.html" class="nav-item nav-link">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="projects.html" class="nav-item nav-link">FAQ</a>
+                    </li>
+                </ul>
+                <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a href="contact.html" class="nav-item nav-link">Login</a></li>
+                    </li>
+                    <li class="nav-item">
+                        <a href="contact.html" class="nav-item nav-link">SignUp</a></li>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
