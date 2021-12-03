99b4c86761fc1264b48c32671fde45f21dbbce95
Franco de Guzman <tangkekofranco06@gmail.com>
2021-12-03 21:55:13 +0800

    modified navbar for logged in user

--- application/views/OnlineFreelancingServices/inc/navbar.php
+++ application/views/OnlineFreelancingServices/inc/navbar.php
@@ -2,26 +2,35 @@
 <?php 
 if($this->session->userdata('UserLoginSession')){
 ?>
-    <nav>
-        <img src="">
-        <button onclick="window.location.href='NewsFeed';">
-            NewsFeed
-        </button>
-
-        <a style="cursor:pointer;color:blue;" href="Profilepage">
-            Profile
-        </a>
-        
-        <button onclick="window.location.href='Logout';">
-            Logout
-        </button>
-    </nav>    
+    <nav class="navbar custom-navbar navbar-expand-md navbar-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                <img class="img-fluid" src="<?php echo base_url();?>public/images/logo.png" alt="" width="120" class="d-inline-block align-text-top">
+                </a>
+                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <ul class="nav justify-content-left collapse navbar-collapse" id="navbarCollapse">
+                    <li class="nav-item">
+                        <a href="NewsFeed" class="nav-item active nav-link">NewsFeed</a></li>
+                    </li>
+                    <li class="nav-item">
+                        <a href="Profilepage" class="nav-item nav-link">Profile</a>
+                    </li>
+                </ul>
+                <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a href="Logout" class="nav-item nav-link">Logout</a>
+                    </li>
+                </ul>
+            </div>
+    </nav>
 <?php
 }else{
 ?>
     
     <nav class="navbar custom-navbar navbar-expand-md navbar-dark">
-            <div class="container-fluid ">
+            <div class="container-fluid">
                 <a class="navbar-brand" href="#">
                 <img class="img-fluid" src="<?php echo base_url();?>public/images/logo.png" alt="" width="120" class="d-inline-block align-text-top">
                 </a>
