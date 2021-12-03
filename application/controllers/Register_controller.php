946d7b9d8bfbb1588ce887619d9b0fef5f7d8570
iansisante <iansisante@gmail.com>
2021-12-03 16:46:55 +0800

    form validation agen

--- application/controllers/Register_controller.php
+++ application/controllers/Register_controller.php
@@ -9,26 +9,25 @@ class Register_controller extends CI_Controller {
         parent::__construct();
         $this->load->helper('url');
         $this->load->model('OFS/Register_model');     
+        $this->load->library('form_validation');
     }
 
     public function addUser() {    
 
         if($_SERVER['REQUEST_METHOD']=='POST'){
+            echo 'asd';
             
             $this->form_validation->set_rules('first_name','First name','alpha|trim|required');
             $this->form_validation->set_rules('last_name','Last name','alpha|trim|required');
-            $this->form_validation->set_rules('middle_name','Middle name','alpha|trim|required');
+            $this->form_validation->set_rules('middle_name','Middle name','alpha|trim');
             $this->form_validation->set_rules('contact','Contact Number','trim|required|integer');
-            $this->form_validation->set_rules('password','Password','trim|min_length[6]|max_length[20]|required');
+            $this->form_validation->set_rules('password','Password','trim|min_length[5]|max_length[20]|required');
             $this->form_validation->set_rules('confirm-pw','Password','trim|matches[password]|required');
 
-            $this->form_validation->set_rules('email','Email','required|valid_email|is_unique[users.email]',
-                array(
-                    'is_unique' => 'This %s already exists.'
-                )
-            );
+            $this->form_validation->set_rules('email','Email','trim|required|valid_email|is_unique[users.email]');
 
             if($this->form_validation->run()==TRUE){
+
                 $fn = $this->input->post('first-name');
                 $sn = $this->input->post('last-name');
                 $mn = $this->input->post('middle-name');
@@ -49,14 +48,19 @@ class Register_controller extends CI_Controller {
                 
                 $insert = $this->Register_model->addUser($data);            
                 
+                
+                echo 'adfs';
+
                 if ($insert) {
                     $this->session->set_flashdata('success', '');
                     redirect(base_url('index.php/OnlineFreelancingServices/Loginpage'));
 
                 }
-                else   
-                    redirect(base_url('index.php/OnlineFreelancingServices/Registerpage'));
-
+                    
+            }else {
+                $this->session->set_flashdata('error','Please fill the fields correctly.');
+                $this->load->helper('url');
+                redirect(base_url('index.php/Registerpage'));
             }
         
         }
