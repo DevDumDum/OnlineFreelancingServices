05e223a8750a91cec6742e8a40ea2c90ed76da8b
MakuAren <69324542+MakuAren@users.noreply.github.com>
2021-12-03 23:19:16 +0800

    updated navbar, fixed some parameters on login andd register

--- application/controllers/OnlineFreelancingServices.php
+++ application/controllers/OnlineFreelancingServices.php
@@ -48,21 +48,20 @@ class OnlineFreelancingServices extends CI_Controller{
         $this->load->helper('url');
         $this -> load -> view ('OnlineFreelancingServices/inc/header');
         $this -> load -> view ('OnlineFreelancingServices/Login');
-        if($_SERVER['REQUEST_METHOD']=='POST')
-		{
+        if($_SERVER['REQUEST_METHOD']=='POST'){
+            $array_items = array('id' => '', 'email' => '');
+            $this->session->unset_userdata($array_items);
 			$this->form_validation->set_rules('email','Email','required');
 			$this->form_validation->set_rules('password','Password','required');
 
-			if($this->form_validation->run()==TRUE)
-			{
+			if($this->form_validation->run()==TRUE){
 				$email = $this->input->post('email');
 				$password = $this->input->post('password');
 				
 
 				$this->load->model('OFS/OFS_model');
 				$status = $this->OFS_model->checkPassword($password,$email);
-				if($status!=false)
-				{
+				if($status!=false){
 					$email = $status->email;
 
 					$session_data = array(
@@ -74,17 +73,12 @@ class OnlineFreelancingServices extends CI_Controller{
 
 					$this->load->helper('url');
                     redirect(base_url('index.php/NewsFeed'));
-				}
-				else
-				{
+				}else{
 					$this->session->set_flashdata('error','Email or Password is Wrong');
                     $this->load->helper('url');
                     redirect(base_url('index.php/Loginpage'));
 				}
-
-			}
-			else
-			{
+			}else{
 				$this->session->set_flashdata('error','Fill all the required fields');
 				$this->load->helper('url');
                 redirect(base_url('index.php/Loginpage'));
