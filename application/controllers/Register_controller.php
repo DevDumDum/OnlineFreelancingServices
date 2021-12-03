5c212b4056671819d06cfa3f3decfd71f61b9fd5
iansisante <iansisante@gmail.com>
2021-12-03 15:52:44 +0800

    form validation

--- application/controllers/Register_controller.php
+++ application/controllers/Register_controller.php
@@ -11,34 +11,56 @@ class Register_controller extends CI_Controller {
         $this->load->model('OFS/Register_model');     
     }
 
-    public function addUser() {
-        
-            $fn = $this->input->post('first-name');
-            $sn = $this->input->post('last-name');
-            $mn = $this->input->post('middle-name');
-            $contact = $this->input->post('contact');
-            $email = $this->input->post('email-address');
-            $password = $this->input->post('password');
-            $status = true;
+    public function addUser() {    
 
-            $data = array (
-                'first_name' => $fn,
-                'last_name' => $sn,
-                'middle_name' => $mn,
-                'name' => $fn." ".$mn." ".$sn,
-                'contact' => $contact,
-                'email' => $email,
-                'password' => $password,
-                'status' => $status
-            );
+        if($_SERVER['REQUEST_METHOD']=='POST'){
             
-            $insert = $this->Register_model->addUser($data);
-            redirect(base_url('index.php/OnlineFreelancingServices/Loginpage'));
+            $this->form_validation->set_rules('first_name','First name','alpha|trim|required');
+            $this->form_validation->set_rules('last_name','Last name','alpha|trim|required');
+            $this->form_validation->set_rules('middle_name','Middle name','alpha|trim|required');
+            $this->form_validation->set_rules('contact','Contact Number','trim|required|integer');
+            $this->form_validation->set_rules('password','Password','trim|min_length[6]|max_length[20]|required');
+            $this->form_validation->set_rules('confirm-pw','Password','trim|matches[password]|required');
 
-            if ($insert) {
+            $this->form_validation->set_rules('email','Email','required|valid_email|is_unique[users.email]',
+                array(
+                    'is_unique' => 'This %s already exists.'
+                )
+            );
+
+            if($this->form_validation->run()==TRUE){
+                $fn = $this->input->post('first-name');
+                $sn = $this->input->post('last-name');
+                $mn = $this->input->post('middle-name');
+                $contact = $this->input->post('contact');
+                $email = $this->input->post('email-address');
+                $password = $this->input->post('password');
+                $status = true;
+
+                $data = array (
+                    'first_name' => $fn,
+                    'last_name' => $sn,
+                    'middle_name' => $mn,
+                    'contact' => $contact,
+                    'email' => $email,
+                    'password' => $password,
+                    'status' => $status
+                );
                 
+                $insert = $this->Register_model->addUser($data);            
+                
+                if ($insert) {
+                    $this->session->set_flashdata('success', '');
+                    redirect(base_url('index.php/OnlineFreelancingServices/Loginpage'));
+
+                }
+                else   
+                    redirect(base_url('index.php/OnlineFreelancingServices/Registerpage'));
+
             }
-            else   
-                redirect(base_url('OnlineFreelancingServices/Registerpage'));
+        
+        }
+        
+
     }
 }
