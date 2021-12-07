f95c56b1f77fb9dc2fe6192c8b9b35213c77caa2
iansisante <iansisante@gmail.com>
2021-12-08 02:31:57 +0800

    improved error messages
    may revert this commit

--- application/controllers/Register_controller.php
+++ application/controllers/Register_controller.php
@@ -12,66 +12,57 @@ class Register_controller extends CI_Controller {
     }
 
     public function addUser() {
-        var_dump($this->session->flashdata('error'));
-        var_dump($this->session->flashdata('success'));
 
         if($_SERVER['REQUEST_METHOD']=='POST'){
 
             $this->form_validation->set_rules('email-address','Email','trim|required|valid_email|is_unique[users.email],');
-        
-            if($this->form_validation->run()==TRUE)  {
-                $this->form_validation->set_rules('first-name','First name','alpha|trim|required');
-                $this->form_validation->set_rules('last-name','Last name','alpha|trim|required');
-                $this->form_validation->set_rules('middle-name','Middle name','alpha|trim|required');
-                $this->form_validation->set_rules('contact','Contact Number','integer|trim|required');
-                $this->form_validation->set_rules('password','Password','trim|required');
-                $this->form_validation->set_rules('confirm-pw','Password','trim|required|matches[password]');
+            $this->form_validation->set_rules('first-name','First name','alpha|trim|required');
+            $this->form_validation->set_rules('last-name','Last name','alpha|trim|required');
+            $this->form_validation->set_rules('middle-name','Middle name','alpha|trim|required');
+            $this->form_validation->set_rules('contact','Contact Number','integer|trim|required');
+            $this->form_validation->set_rules('password','Password','trim|required');
+            $this->form_validation->set_rules('confirm-pw','Password','trim|required|matches[password]');
 
-                if($this->form_validation->run()==TRUE){
-                    $fn = $this->input->post('first-name');
-                    $sn = $this->input->post('last-name');
-                    $mn = $this->input->post('middle-name');
-                    $contact = $this->input->post('contact');
-                    $email = $this->input->post('email-address');
-                    $password = $this->input->post('password');
-                    $status = true;
+            if($this->form_validation->run()==TRUE){
+                $fn = $this->input->post('first-name');
+                $sn = $this->input->post('last-name');
+                $mn = $this->input->post('middle-name');
+                $contact = $this->input->post('contact');
+                $email = $this->input->post('email-address');
+                $password = $this->input->post('password');
+                $status = true;
 
-                    $data = array (
-                        'first_name' => $fn,
-                        'last_name' => $sn,
-                        'middle_name' => $mn,
-                        'contact' => $contact,
-                        'email' => $email,
-                        'password' => $password,
-                        'status' => $status
-                    );
+                $data = array (
+                    'first_name' => $fn,
+                    'last_name' => $sn,
+                    'middle_name' => $mn,
+                    'contact' => $contact,
+                    'email' => $email,
+                    'password' => $password,
+                    'status' => $status
+                );
 
-                    if ($this->Register_model->addUser($data)) {
-                        $this->load->helper('url');
-                        $this->load->model('OFS/OFS_model');
-                        $status = $this->OFS_model->checkPassword($password,$email);
-                        if($status!=false){
-                            $session_data = array(
-                                'email'=>$email,
-                                'id'=>$id,
-                            );
-                            $this->session->set_userdata('UserLoginSession',$session_data);
-                        }
-                        redirect(base_url('index.php/NewsFeed'));
-                    }else{
-                        $this->load->helper('url');
-                        $this->session->set_flashdata('error','Error query.');
-                    }
-                }else {                
+                if ($this->Register_model->addUser($data)) {
                     $this->load->helper('url');
-                    $this->session->set_flashdata('error','Please fill the fields correctly.');
+                    $this->load->model('OFS/OFS_model');
+                    $status = $this->OFS_model->checkPassword($password,$email);
+                    if($status!=false){
+                        $session_data = array(
+                            'email'=>$email,
+                            'id'=>$id,
+                        );
+                        $this->session->set_userdata('UserLoginSession',$session_data);
+                    }
+                    redirect(base_url('index.php/NewsFeed'));
+                }else{
+                    $this->load->helper('url');
+                    $this->session->set_flashdata('error','Error query.');
                 }
             }else {
-                $this->load->helper('url');
-                $this->session->set_flashdata('error','Account existing or email in not correct format.');
+                $this->load->view('OnlineFreelancingServices/Register.php');
             }
-            redirect(base_url('index.php/Registerpage'));
-        }
+
+        }        
     }
 
     public function addMod() {
@@ -105,6 +96,7 @@ class Register_controller extends CI_Controller {
                         $this->session->set_userdata('UserLoginSession',$session_data);
                     }
                     redirect(base_url('Dashboardpage_mod'));
+
                 }else{
                     $this->load->helper('url');
                     $this->session->set_flashdata('error','Error query.');
--- application/views/Admin/Register.php
+++ application/views/Admin/Register.php
@@ -12,30 +12,31 @@
 
 
     <div class = "form-group">
-        <label for="companyid" class="label-default">Company ID:</label>
-        <input class="form-control" name="companyid" id="companyid" type="text" value="<?php echo set_value('companyid');?>">
+      <label for="companyid" class="label-default">Company ID:</label>
+      <input class="form-control" name="companyid" id="companyid" type="text" value="<?php echo set_value('companyid');?>">
+      <?php echo form_error('companyid'); ?>    
     </div>
 
     <div class = "form-group">
-        <label for="" class="label-default">Password:</label>
-        <input class="form-control" name="password" id="password" type="password" value="<?php echo set_value('password');?>">
+      <label for="" class="label-default">Password:</label>
+      <input class="form-control" name="password" id="password" type="password" value="<?php echo set_value('password');?>">
+      <?php echo form_error('password'); ?>   
     </div>
 
     <div class = "form-group">
-        <label for="" class="label-default">Confirm Password:</label>
-        <input class="form-control" name="password2" id="password" type="password" value="<?php echo set_value('password2');?>">
+      <label for="" class="label-default">Confirm Password:</label>
+      <input class="form-control" name="password2" id="password" type="password" value="<?php echo set_value('password2');?>">
+      <?php echo form_error('password2'); ?>   
     </div>
 
     <div class = "form-group">
-        <button type="submit" class="btn btn-primary" name="register">Register</button>    
+      <button type="submit" class="btn btn-primary" name="register">Register</button>    
     </div>
 
     <button type="submit" onclick="window.location.href='Login';">
       Back
     </button>
-
-    <?php echo validation_errors(); ?>    
-
+    
   </form>
 
 
