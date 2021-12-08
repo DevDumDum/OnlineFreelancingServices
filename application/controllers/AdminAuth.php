308fbe5953def3b06062a935ea7e2db1f8008d07
MakuAren <69324542+MakuAren@users.noreply.github.com>
2021-12-08 20:59:42 +0800

    added default index redirect

--- application/controllers/AdminAuth.php
+++ application/controllers/AdminAuth.php
@@ -3,6 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 class AdminAuth extends CI_Controller{
+    public function index(){
+        redirect(base_url('AdminAuth/Dashboard'));
+    }
 
     public function AdminLogin(){
         
