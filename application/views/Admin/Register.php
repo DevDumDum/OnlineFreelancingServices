fc2d0c13a7c028f077eebe92f06c13cd4d416dd3
Franco de Guzman <tangkekofranco06@gmail.com>
2021-12-09 23:52:52 +0800

    registerpage

--- application/views/Admin/Register.php
+++ application/views/Admin/Register.php
@@ -1,62 +1,68 @@
 <!doctype html>
 <html lang="en">
-<link rel="stylesheet" href="<?php echo base_url();?>public/css/adminnavbar.css"/>
-  <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="<?php echo base_url();?>public/css/moderatorRegisterpage.css"/>
+<link rel="stylesheet" href="<?php echo base_url();?>public/css/adminnavbar.css" />
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+	integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <header>
-    <nav class="navbar custom-navbar navbar-expand-md navbar-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="Homepage">
-                <img class="img-fluid" src="<?php echo base_url();?>public/images/logo.png" alt="" width="120" class="d-inline-block align-text-top">
-                </a>
-                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <ul class="nav justify-content-end collapse navbar-collapse " id="navbarCollapse">
-                    <div class="nav-item d-flex justify-content-left ">
-                        <img class="img-fluid" src="<?php echo base_url();?>public/images/changepassword.png" alt="" width="13%">
-                        <a href="Loginpage" class="nav-link-custom">Change Password</a>
-                    </div>
-                    <div class="nav-item d-flex justify-content-left">
-                        <img class="img-fluid" src="<?php echo base_url();?>public/images/logout.png" alt="" width="17%">
-                        <a href="Registerpage" class="nav-link-custom">Log Out</a>
-                    </div>
-                </ul>
-            </div>
-    </nav>
+	<nav class="navbar custom-navbar navbar-expand-md navbar-dark">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="Homepage">
+				<img class="img-fluid" src="<?php echo base_url();?>public/images/logo.png" alt="" width="120"
+					class="d-inline-block align-text-top">
+			</a>
+			<ul class="nav justify-content-end">
+				<li class="nav-item">
+					<img class="img-fluid" src="<?php echo base_url();?>public/images/help.png" alt="" width="50">
+				</li>
+			</ul>
+		</div>
+	</nav>
 </header>
 
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Moderator Register</title>
-  </head>
-  <body>
-    <h1>Moderator Register</h1>
-    <div class = "form-group">
-        <label for="companyid" class="label-default">Company ID:</label>
-        <input class="form-control" name="companyid" id="companyid" type="text">
-    </div>
-
-    <div class = "form-group">
-        <label for="" class="label-default">Password:</label>
-        <input class="form-control" name="password" id="password" type="password">
-    </div>
-
-    <div class = "form-group">
-        <label for="" class="label-default">Confirm Password:</label>
-        <input class="form-control" name="password2" id="password" type="password">
-    </div>
-
-    <div class = "form-group">
-        <button class="btn btn-primary" name="register">Register</button>    
-    </div>
-
-    <button onclick="window.location.href='Login';">
-      Back
-    </button>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Moderator Register</title>
+</head>
+<body>
+  <h1 class="text-center">Moderator Register</h1>
+  <div class="container">
+      <div class="mb-3 mb-3_custom">
+						<div class="form-text-company-id-padding">
+							<label for="companyid" class="form-label ">Company ID:</label>
+						</div>
+						<div class="form-padding">
+							<input type="form-control" name="companyid" class="form-control form-width" id="companyid" type="text">
+						</div>
+		  </div>
+		  <div class="mb-3 mb-3_custom">
+						<div class="form-text-password-padding">
+							<label for="" class="form-label">Password</label>
+						</div>
+						<div class="form-padding">
+							<input type="password" name="password" class="form-control form-width" id="password">
+						</div>
+		  </div>
+      <div class="mb-3 mb-3_custom">
+						<div class="form-text-password-padding">
+							<label for="" class="form-label">Confirm Password:</label>
+						</div>
+						<div class="form-padding">
+							<input type="password" name="password2" class="form-control form-width" id="password">
+						</div>
+		  </div>
+			    <div class="text-center text-button">
+            <button type="button" class="btn btn-light border border-primary back-custom-color"><a href="Homepage">BACK</a></button>
+            <button type="submit" class="btn btn-primary button-custom-color">Register</button>
+		      </div>
+  </div>
+</body>
 
 <!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+	integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
 </html>
