cfbf2502a7dd12e7e163a2e964f59cdb2541d1f9
RachelleAgapor <agaporrachelle@gmail.com>
2021-12-10 17:55:29 +0800

    Dashboard with (temporary) navbar

--- application/views/Admin/Dashboard.php
+++ application/views/Admin/Dashboard.php
@@ -16,17 +16,36 @@ else
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../public/css/dashboard.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>public/css/adminnavbar.css"/>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Dashboard</title>
   </head>
+
+  <!-- Admin Navbar (temporary) -->
+  <nav class="navbar custom-navbar navbar-expand-md navbar-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="Homepage">
+                <img class="img-fluid" src="<?php echo base_url();?>public/images/logo.png" alt="" width="120" class="d-inline-block align-text-top">
+                </a>
+                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <ul class="nav justify-content-end collapse navbar-collapse " id="navbarCollapse">
+                    <li class="nav-item d-flex justify-content-left ">
+                        <img class="img-fluid" src="<?php echo base_url();?>public/images/changepassword.png" alt="" width="13%">
+                        <a href="Loginpage" class="nav-link-custom">Change Password</a>
+                    </li>
+                    <li class="nav-item d-flex justify-content-left">
+                        <img class="img-fluid" src="<?php echo base_url();?>public/images/logout.png" alt="" width="17%">
+                        <a href="AdminLogout" class="nav-link-custom">Log Out</a>
+                    </li>
+                </ul>
+            </div>
+    </nav>
+
   <body>
     <?php echo $udata['user_type'];?>
-    <br>
-    <br>
-    <button onclick="window.location.href='';">
-    Change Password
-    </button>
-
-    <button type="button" class="btn btn-primary button-custom-color"><a href="AdminLogout">Logout</a></button>
 
     <div class="AdminLP">
         <img src="<?php echo base_url();?>public/images/logo.png" width="100%">
