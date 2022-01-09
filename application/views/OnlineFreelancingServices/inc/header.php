b6fc5dfa88788fe11cdc3ad77787437c4e94489e
Ali <azperps@gmail.com>
2022-01-10 00:18:00 +0800

    made the contact us page (php and css file), added some image. changed the logo into png from gif. SUBJECT FOR CHECKING

--- application/views/OnlineFreelancingServices/inc/header.php
+++ application/views/OnlineFreelancingServices/inc/header.php
@@ -21,6 +21,7 @@ if($this->session->userdata('page')){
     <link rel="stylesheet" href="<?php echo base_url();?>public/css/register.css"/>
     <link rel="stylesheet" href="<?php echo base_url();?>public/css/loginpage.css"/>
     <link rel="stylesheet" href="<?php echo base_url();?>public/css/homepage.css"/>
+    <link rel="stylesheet" href="<?php echo base_url();?>public/css/contactus.css"/>
     <link rel="stylesheet" href="<?php echo base_url();?>public/css/mediaquery.css"/>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,500;0,600;1,300&display=swap" rel="stylesheet">
     
