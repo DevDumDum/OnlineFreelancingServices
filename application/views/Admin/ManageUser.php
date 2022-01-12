11ea754ab2f257d79f3ba15657a23d95af8477c8
Franco de Guzman <tangkekofranco06@gmail.com>
2022-01-13 00:24:52 +0800

    RESPONSIVENESS OF ADMINPAGE FROM DASHBOARD TO VERIFY USERS ONLY

--- application/views/Admin/ManageUser.php
+++ application/views/Admin/ManageUser.php
@@ -1,54 +1,86 @@
-<body>   
-<h2>Manage User</h2>
-
-<table>
-    <tr>
-    <th>User</th>
-    <th> </th>
-    </tr>
-
-    <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>
-        <button class="editbtn">Activate</button>
-        <button class="editbtn">Deactivate</button>
-    </td></tr>
-    
-    <tr>
-    <td>Centro comercial</td>
-    <td>
-        <button class="editbtn">Activate</button>
-        <button class="editbtn">Deactivate</button>
-    </td></tr>
-
-    <tr>
-    <td>Ernst Handel</td>
-    <td>
-        <button class="editbtn">Activate</button>
-        <button class="editbtn">Deactivate</button>
-    </td></tr>
-
-    <tr>
-    <td>Island Trading</td>
-    <td>
-        <button class="editbtn">Activate</button>
-        <button class="editbtn">Deactivate</button>
-    </td></tr>
- 
-</table>
-
-    <button onclick="window.location.href='Dashboard';">
-    Back
-    </button>
-
-    <button onclick="window.location.href='';">
-    Moderator
-    </button>
-
-    <button onclick="window.location.href='';">
-    User
-    </button>
-
+<body>
+    <!---POP UP-->
+    <div id="hiddenbox">
+        <div id="bg_box">
+            <div class="modal-header">
+                <div class="title">User</div>
+                    <button class="close-button" onclick="hidebox()">&times;</button>
+                </div>
+            </div>
+        <div id="blackbox" onclick="hidebox()">
+        </div>
+    </div>
+    <!--BACK BUTTON-->
+    <div class="container">
+    <button class="btn" onclick="window.location.href='<?php echo base_url('AdminAuth/Dashboard');?>';">Back</button>
+        <!---DASHBOARD BUTTONS-->
+        <div class="dashboardButtons">
+            <h1>Manage User Status</h1>
+        </div>
+        <!---NEW USER TABLE-->
+        <div class="tables">
+            <table class = "table table-dark table-hover center">
+                <tr>
+                <th>User</th>
+                <th> </th>
+                </tr>
+                
+                <div>
+                <tr>
+                <td onclick="newDetails()">
+                    <span>Alfreds Futterskie</span>
+                </td>
+                </div>
+                <td class="status">
+                    <button class="editbtn1" style="cursor: pointer;" id="activate">Activate</button>
+                    <button class="editbtn2" style="cursor: pointer;" id="deactivate">Deactivate</button>
+                </td></tr>
+            
+                <tr>
+                <td onclick="newDetails()">
+                    <span>Centro comercial</span>
+                </td>                
+                <td class="status">
+                    <button class="editbtn1" style="cursor: pointer;">Activate</button>
+                    <button class="editbtn2" style="cursor: pointer;">Deactivate</button>
+                </td></tr>
+            
+                <tr>
+                <td onclick="newDetails()">
+                    <span>Ernst Handel</span>
+                </td>
+                <td class="status">
+                    <button class="editbtn1" style="cursor: pointer;">Activate</button>
+                    <button class="editbtn2" style="cursor: pointer;">Deactivate</button>
+                </td></tr>
+            
+                <tr>
+                <td onclick="newDetails()">
+                    <span>Island Trading</span>
+                </td>                
+                <td class="status">
+                    <button class="editbtn1" style="cursor: pointer;" >Activate</button>
+                    <button class="editbtn2" style="cursor: pointer;">Deactivate</button>
+                </td></tr>
+             
+            </table>
+        </div>
+    </div>
+    <script>
+        function newDetails(){
+            document.getElementById("hiddenbox").style.display="block";
+            document.getElementById("hiddenbox").style.animation="fadebox .3s reverse linear";
+        }
+        function hidebox(){
+            document.getElementById("hiddenbox").style.display="none";
+        }
+    </script>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <div class="btn_category">
+            <button class="btn" onclick="window.location.href='';">Moderator</button>
+            <button class="btn" onclick="window.location.href='';">User</button>
+    </div>
 </body>
 </html>
 
