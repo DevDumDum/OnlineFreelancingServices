7c081d41395de0f3d3fd13a73f86ebe1454f97dd
Franco de Guzman <tangkekofranco06@gmail.com>
2022-01-14 20:07:35 +0800

    Re-locate Moderator/User filter position

--- application/views/Admin/ManageUser.php
+++ application/views/Admin/ManageUser.php
@@ -16,6 +16,10 @@
         <!---DASHBOARD BUTTONS-->
         <div class="dashboardButtons">
             <h1>Manage User Status</h1>
+            <div class="btn_category">
+            <button class="btn" onclick="window.location.href='';">Moderator</button>
+            <button class="btn" onclick="window.location.href='';">User</button>
+    </div>
         </div>
         <!---NEW USER TABLE-->
         <div class="tables">
@@ -77,10 +81,6 @@
     </script>
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <div class="btn_category">
-            <button class="btn" onclick="window.location.href='';">Moderator</button>
-            <button class="btn" onclick="window.location.href='';">User</button>
-    </div>
 </body>
 </html>
 
