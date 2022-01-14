fa6266573331a67da4c34887afc2fda7ab6f8757
Ali <azperps@gmail.com>
2022-01-15 02:37:01 +0800

    [Unfinished Task] created the containers, edited the css for edit profile button, status. I'll add the calendar that aileen made on a diff file later.

--- application/views/OnlineFreelancingServices/Profile.php
+++ application/views/OnlineFreelancingServices/Profile.php
@@ -22,9 +22,66 @@
                 </div>
                 <h1 class="profileUsername">Name</h1>
                 <!---LOCATION---->
-                <button type="button" id ="editProfile" >Edit Profile</button>
-
+                <button class="editbutton" type="button" id ="editProfile">
+                    <i class="editbuttonicon fal fa-pencil"></i>Edit Profile
+                </button>
+                <div class="switchBox">
+                    <label class="switch">
+                        <input type="checkbox" checked>
+                        <span class="slider">
+                        <h4 id = "h4-status">Status</h4></span>
+                    </label>
+                </div>
             </div>
+
+            <div class="col-md-9" style="float:right">
+                <button type="button" class="messageprofilebtn btn btn-primary btn-sm">Message</button>
+                <div class="profileDesc">
+                    <label for="validationTextarea" class="form-label">Add Description</label>
+                    <textarea class="form-control" id="validationTextarea" placeholder="Type here..." required></textarea>
+                        <div class="descbuttons">
+                            <button class="descprofilebtn btn btn-outline-secondary btn-sm" type="submit">Edit</button>
+                            <button class="descprofilebtnsave btn btn-dark btn-sm" type="submit">Save</button>
+                        </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 profileProjwrapper">
+                <div class="profileProjContainer">
+                    
+                    <div class="row row-cols-1 row-cols-md-3 g-4">
+                        <div class="col">
+                            <div class="card">
+                            <img src="#" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card">
+                            <img src="#" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card">
+                            <img src="#" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            
         </div>
 
     </div>
@@ -32,14 +89,9 @@
     
 
     <!--
-    <button type="button" id ="editProfile" >Edit Profile</button> 
 
     <div class = "container-status"></div>
-    <label class="switch">
-        <input type="checkbox" checked>
-            <span class="slider"></span>
-            <h4 id = "h4-status">Status</h4>
-    </label>
+    
 
     <div class="container-description">
         <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at congue urna. Vivamus gravida congue velit, nec dapibus leo hendrerit vel. Mauris consectetur et ex ac rutrum. Maecenas interdum sagittis leo ac commodo. Vivamus velit augue, imperdiet eu laoreet a, mattis a lacus. Nam sagittis massa malesuada ante mattis maximus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus volutpat metus vitae ipsum blandit vulputate. Morbi auctor urna interdum, auctor eros vitae, tristique orci. Praesent pharetra urna tristique orci bibendum fermentum. Aenean faucibus justo ac ipsum aliquam convallis. Aenean ultricies eget libero et ultrices. Nullam auctor nisl ac magna interdum, et consectetur est blandit. Proin eu gravida purus. In quis efficitur nibh. Vestibulum congue euismod massa in tristique.</i>
