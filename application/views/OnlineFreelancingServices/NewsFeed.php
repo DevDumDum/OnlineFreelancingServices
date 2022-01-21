0b066329ce0160895dd99d4c02c84b71156d710c
Franco de Guzman <tangkekofranco06@gmail.com>
2022-01-21 22:21:52 +0800

    NEWSFEED ELEMENTS POSITIONING AND DESIGN (PARTIAL) DONE

--- application/views/OnlineFreelancingServices/NewsFeed.php
+++ application/views/OnlineFreelancingServices/NewsFeed.php
@@ -15,64 +15,82 @@ else
 <body id="newsfeed">
     <div class="container-newsfeed">
         <div class="row">
-            <div class="col pl-0 border border-primary work-category-side sticky-top">
+            <div class="col-3 pl-0 work-category-side sticky-top">
                 <!-- for filtering category-->
                 <!--Work:-->
-                Type of Work:
-                <select name="Work">
-                    <option value="null">----</option>
-                        <?php if(!empty($key_works)) { foreach($key_works as  $w){ ?>
-                            <option value="<?php echo $w['ID'];?>"> <?php echo $w['profession_type'];?> </option>
-                        <?php }} ?>
-                </select><br><br>
-
-                <!--Location:-->
-                Location:<br>
-                Province: <input type="text" name="province"><br>
-                City: <input type="text" name="City"><br>
-                <button name="category">Apply</button>
+                <div class="card bg-light mb-3">
+                    <div class="card-header">Work Category Filter</div>
+                        <div class="card-body">
+                            <p>
+                            Type of Work:
+                            <select name="Work">
+                                <option value="null">----</option>
+                                    <?php if(!empty($key_works)) { foreach($key_works as  $w){ ?>
+                                        <option value="<?php echo $w['ID'];?>"> <?php echo $w['profession_type'];?> </option>
+                                    <?php }} ?>
+                            </select>
+                            </p>             
+                        <!--Location:-->
+                        <div class="location-filter">
+                            <p>Location: <input type="text" name="location"></p>
+                            <p>Province: <input type="text" name="province"></p>
+                            <p>City: <input type="text" name="City"></p> 
+                        </div>
+                        <button type="button" class="btn btn-success work-category-apply" name="category">Apply</button> 
+                    </div>
+                </div>
             </div>
-            <div class="col-9 border border-warning">
+            <div class="col-8 newsfeed-side">
                 <!--AddPost button display create post at line 23 event-->
-                <button onclick="AddPostPopUp()">Add Post</button>
+                <div class="card bg-light mb-3 card-width">
+                    <div class="card-header"><h1>Finding A Job? A worker? Post now!</h1></div>
+                        <div class="card-body">
+                        <button type="button" class="btn btn-primary btn-lg" onclick="AddPostPopUp()">Add Post</button>
+                    </div>
+                </div>
                 <!--PopUp createPost-->
                 <div id="hiddenbox-nf">
                     <div id="bg_box-nf">
                         <div class="modal-header-nf">
-                                <h1>Create Post</h1>
-                                <button class="close-button" onclick="hidebox()">&times;</button>
+                            <div class="d-flex justify-content-between">
+                                <div class="p-2"><h1>Create Post</h1></div>
+                                <div class="p-2"><button type="button" class="btn btn-secondary btn-lg rounded-circle" class="close-button" onclick="hidebox()">&times;</button></div>
+                            </div>
                         </div>
                         <div class="create-post">
                                 <form action="<?=base_url('Post_controller/addPost')?>" method="post" enctype="multipart/form-data">
-                                    <div class="add-post-content">
+                                    <div class="add-post-content btn-block">
                                         <input type="text" name="poster_name" value="<?php echo $udata['id'];?>" style="display:none">
-                                            <label for="">Work Category</label>
-                                            <select name="work" id="works">
-                                                <?php if(!empty($key_works)) { foreach($key_works as  $w){ ?>
-                                                    <option value="<?php echo $w['ID'];?>"> <?php echo $w['profession_type'];?> </option>
-                                                <?php }} ?>
-                                            </select>
-                                            <button name="addWorkPost">+</button>
-                                        <label for="">Description</label>
-                                        <input type="text" name="description" id="desc" placeholder="Requirements" required /><br>
+                                            <p>
+                                                <label for="">Work Category</label>
+                                                <select name="work" id="works">
+                                                    <?php if(!empty($key_works)) { foreach($key_works as  $w){ ?>
+                                                        <option value="<?php echo $w['ID'];?>"> <?php echo $w['profession_type'];?> </option>
+                                                    <?php }} ?>
+                                                </select>
+                                                <button type="button" class="btn btn-secondary" name="addWorkPost">+</button><br>
 
-                                        <label for="">Worker(s) needed</label>
-                                        <input type="number" name="worker-count" id="worker_count" value="1" max="100" min="1" oninput="this.value = !!this.value && Math.abs(this.value) >= 1 ? Math.abs(this.value) : null"><br>
-                                        
-                                        <label for="">Location</label>
-                                        <input type="text" name="location" id="location" placeholder="Work location" required /><br>
+                                                <label for="">Description</label>
+                                                <input type="text" name="description" id="desc" placeholder="Requirements" required /> <br>
 
-                                        <label for="">Minimum Payment</label>
-                                        <input type="number" name="min-pay" id="min_pay" value="" max="100" min="1" placeholder="None" disabled oninput="this.value = !!this.value && Math.abs(this.value) >= 1 ? Math.abs(this.value) : null">
-                                        
-                                        <label for="">Fixed</label>
-                                        <input type="checkbox" id="min-checker" checked onclick="set_min_pay(this)"><br>
-                                        
-                                        <label for="">Maximum Payment</label>
-                                        <input type="number" name="max-pay" id="max_pay" oninput="this.value = !!this.value && Math.abs(this.value) >= 1 ? Math.abs(this.value) : null" required /><br>
+                                                <label for="">Worker(s) needed</label>
+                                                <input type="number" name="worker-count" id="worker_count" value="1" max="100" min="1" oninput="this.value = !!this.value && Math.abs(this.value) >= 1 ? Math.abs(this.value) : null">
+                                                
+                                                <label for="">Location</label>
+                                                <input type="text" name="location" id="location" placeholder="Work location" required /> <br>
+                                                
+                                                <label for="">Minimum Payment</label>
+                                                <input type="number" name="min-pay" id="min_pay" value="" max="100" min="1" placeholder="None" disabled oninput="this.value = !!this.value && Math.abs(this.value) >= 1 ? Math.abs(this.value) : null">
+                                                
+                                                <label for="">Fixed</label>
+                                                <input type="checkbox" id="min-checker" checked onclick="set_min_pay(this)"> <br>
+                                                
+                                                <label for="">Maximum Payment</label>
+                                                <input type="number" name="max-pay" id="max_pay" oninput="this.value = !!this.value && Math.abs(this.value) >= 1 ? Math.abs(this.value) : null" required /> 
+                                            </p>
                                         
                                         <input type="file" name="fileToUpload" id="fileToUpload"><br>
-                                        <input type="submit" value="submit" name="submit">
+                                        <input type="button" class="btn btn-block btn-primary btn-sm p-3" value="Post" name="submit">
                                     </div>
                                 </form>
                         </div>
@@ -88,6 +106,8 @@ else
     ====================================================================================================================================
     <br>
     <?php //include("inc/workResult.php"); ?> -->
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 <script>
     function set_min_pay(c){
@@ -107,6 +127,4 @@ else
             document.getElementById("hiddenbox-nf").style.display="none";
         }
 </script>
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </html>
