6a364bb6df59e0c5e43cd899a4955ce49e13723d
iansisante <iansisante@gmail.com>
2022-01-27 22:26:57 +0800

    added full name column

--- db_structure/loginsystem.sql
+++ db_structure/loginsystem.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 25, 2022 at 12:05 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.1
+-- Generation Time: Jan 27, 2022 at 03:08 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -104,6 +104,7 @@ CREATE TABLE `users` (
   `last_name` varchar(255) NOT NULL,
   `first_name` varchar(255) NOT NULL,
   `middle_name` varchar(255) DEFAULT NULL,
+  `full_name` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `contact` int(11) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
@@ -125,29 +126,29 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `user_type`, `last_name`, `first_name`, `middle_name`, `password`, `contact`, `email`, `profession_id`, `feature_list_id`, `education_id`, `location`, `summary`, `calendarlist_id`, `status`, `code`, `jobs`, `posts`, `apply`, `ratings`) VALUES
-(1, 'user', 'Sisante', 'Ian', 'asd', 'asd', 2147483647, 'iansisante@gmail.com', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
-(2, 'user', 'Aren', 'Mark', '', 'asd', 2147483647, 'mark.aren@mail.me', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
-(3, 'admin', 'Minon', 'Alex', 'Benedict', 'qwe', 2147483647, 'me@me.me', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
-(4, 'user', 'Agapor', 'Rachelle', 'Ann', '321', 2147483647, 'rachelle@mail.ann', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
-(5, 'user', 'Guzman', 'Franco', 'De', 'qqq', 2147483647, 'franqq@q.q', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
-(6, 'user', 'dsa', 'asd', 'xxx', 'ddd', 1111111111, 'Anecitatempra@yahoo.com', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
-(9, 'moderator', '', '', NULL, '123', NULL, 'mark.aren@aren.me', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
-(10, 'moderator', '', '', NULL, 'xxx', NULL, 'alex@lex.x', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
-(11, 'moderator', '', '', NULL, '123', NULL, 'bbp@p.b', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
-(14, 'user', 'el', 'al', 'il', '123456', 2147483647, 'boy2pasaway0@gmail.com', '', NULL, NULL, '', NULL, NULL, '0', '0', NULL, NULL, NULL, NULL),
-(16, 'user', 'el', 'al', 'il', '123456', 2147483647, 'yoru@gamil.com', '', NULL, NULL, '', NULL, NULL, '0', '0', NULL, NULL, NULL, NULL),
-(17, 'user', 'sadaw', 'asdaw', 'sadaw', '123456', 2147483647, 'awaw@gaw.com', '', NULL, NULL, '', NULL, NULL, '0', '0', NULL, NULL, NULL, NULL),
-(18, 'user', 'as', 'as', 'wad', '123456', 2147483647, 'yoru@gmail.com', '', NULL, NULL, '', NULL, NULL, '0', '0', NULL, NULL, NULL, NULL),
-(19, 'user', 'el', 'al', 'il', '123456', 2147483647, 'jebronlam@gmail.com', '', NULL, NULL, '', NULL, NULL, '0', '0', NULL, NULL, NULL, NULL),
-(20, 'user', 'el', 'al', 'il', '123456', 2147483647, 'ur@gmail.com', '', NULL, NULL, '', NULL, NULL, '0', '0', NULL, NULL, NULL, NULL),
-(21, 'user', 'as', 'asdaw', 'qwewq', '123456', 2147483647, 'aawy@gmail.com', '', NULL, NULL, '', NULL, NULL, '0', '0', NULL, NULL, NULL, NULL),
-(22, 'user', 'el', 'al', 'il', '123456', 2147483647, 'lomi@gmail.com', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
-(34, 'user', 'el', 'al', 'il', '123456', 2147483647, 'alexandreeminion@gmail.com', '', NULL, NULL, '', NULL, NULL, '1', 'GtQBS3TRmzf1', NULL, NULL, NULL, NULL),
-(36, 'user', 'Sisante', 'Ian', '', '123', 2147483647, 'ian.sisante@tup.edu.ph', '', NULL, NULL, '', NULL, NULL, '1', 'j5QXhb1mFZNx', NULL, NULL, '6', NULL),
-(39, 'user', 'Cabutaje', 'Mark', 'Allen', 'qwe', 2147483647, 'asd@gmail.com', '', NULL, NULL, '', NULL, NULL, '1', 'b4RKhrzCp2uN', NULL, NULL, NULL, NULL),
-(40, 'user', 'kabutski', 'maku', '', 'asd', 982137868, 'mme@mdm.com', '5,11,12', NULL, NULL, '', NULL, NULL, NULL, 'a1r9u7Yxtkq8', NULL, NULL, NULL, NULL),
-(41, 'user', 'carlos', 'juan', '', '123', 947198273, 'markallencabutaje00@gmail.com', '1,5,11', NULL, NULL, '', NULL, NULL, '1', 'YzFGQV4gtorw', NULL, NULL, '1', NULL);
+INSERT INTO `users` (`id`, `user_type`, `last_name`, `first_name`, `middle_name`, `full_name`, `password`, `contact`, `email`, `profession_id`, `feature_list_id`, `education_id`, `location`, `summary`, `calendarlist_id`, `status`, `code`, `jobs`, `posts`, `apply`, `ratings`) VALUES
+(1, 'user', 'Sisante', 'Ian', 'asd', 'Ian asd Sisante', 'asd', 2147483647, 'iansisante@gmail.com', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
+(2, 'user', 'Aren', 'Mark', '', 'Mark Aren', 'asd', 2147483647, 'mark.aren@mail.me', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
+(3, 'admin', 'Minon', 'Alex', 'Benedict', 'Alex Benedict Minon', 'qwe', 2147483647, 'me@me.me', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
+(4, 'user', 'Agapor', 'Rachelle', 'Ann', ' Rachelle Ann Agapor', '321', 2147483647, 'rachelle@mail.ann', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
+(5, 'user', 'Guzman', 'Franco', 'De', 'Franco De Guzman', 'qqq', 2147483647, 'franqq@q.q', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
+(6, 'user', 'dsa', 'asd', 'xxx', 'asd xxx dsa', 'ddd', 1111111111, 'Anecitatempra@yahoo.com', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
+(9, 'moderator', '', '', NULL, '', '123', NULL, 'mark.aren@aren.me', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
+(10, 'moderator', '', '', NULL, '', 'xxx', NULL, 'alex@lex.x', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
+(11, 'moderator', '', '', NULL, '', '123', NULL, 'bbp@p.b', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
+(14, 'user', 'el', 'al', 'il', 'al il el', '123456', 2147483647, 'boy2pasaway0@gmail.com', '', NULL, NULL, '', NULL, NULL, '0', '0', NULL, NULL, NULL, NULL),
+(16, 'user', 'el', 'al', 'il', 'al il el', '123456', 2147483647, 'yoru@gamil.com', '', NULL, NULL, '', NULL, NULL, '0', '0', NULL, NULL, NULL, NULL),
+(17, 'user', 'sadaw', 'asdaw', 'sadaw', 'asdaw sadaw sadaw', '123456', 2147483647, 'awaw@gaw.com', '', NULL, NULL, '', NULL, NULL, '0', '0', NULL, NULL, NULL, NULL),
+(18, 'user', 'as', 'as', 'wad', 'as wad as', '123456', 2147483647, 'yoru@gmail.com', '', NULL, NULL, '', NULL, NULL, '0', '0', NULL, NULL, NULL, NULL),
+(19, 'user', 'el', 'al', 'il', 'al il el', '123456', 2147483647, 'jebronlam@gmail.com', '', NULL, NULL, '', NULL, NULL, '0', '0', NULL, NULL, NULL, NULL),
+(20, 'user', 'el', 'al', 'il', 'al il el', '123456', 2147483647, 'ur@gmail.com', '', NULL, NULL, '', NULL, NULL, '0', '0', NULL, NULL, NULL, NULL),
+(21, 'user', 'as', 'asdaw', 'qwewq', 'asdaw qwewq as', '123456', 2147483647, 'aawy@gmail.com', '', NULL, NULL, '', NULL, NULL, '0', '0', NULL, NULL, NULL, NULL),
+(22, 'user', 'el', 'al', 'il', 'al il el', '123456', 2147483647, 'lomi@gmail.com', '', NULL, NULL, '', NULL, NULL, '1', '0', NULL, NULL, NULL, NULL),
+(34, 'user', 'el', 'al', 'il', 'al il el', '123456', 2147483647, 'alexandreeminion@gmail.com', '', NULL, NULL, '', NULL, NULL, '1', 'GtQBS3TRmzf1', NULL, NULL, NULL, NULL),
+(36, 'user', 'Sisante', 'Ian', '', 'Ian Sisante', '123', 2147483647, 'ian.sisante@tup.edu.ph', '', NULL, NULL, '', NULL, NULL, '1', 'j5QXhb1mFZNx', NULL, NULL, '6', NULL),
+(39, 'user', 'Cabutaje', 'Mark', 'Allen', 'Mark Allen Cabutaje', 'qwe', 2147483647, 'asd@gmail.com', '', NULL, NULL, '', NULL, NULL, '1', 'b4RKhrzCp2uN', NULL, NULL, NULL, NULL),
+(40, 'user', 'kabutski', 'maku', '', 'maku kabutski', 'asd', 982137868, 'mme@mdm.com', '5,11,12', NULL, NULL, '', NULL, NULL, NULL, 'a1r9u7Yxtkq8', NULL, NULL, NULL, NULL),
+(41, 'user', 'carlos', 'juan', '', 'juan carlos', '123', 947198273, 'markallencabutaje00@gmail.com', '1,5,11', NULL, NULL, '', NULL, NULL, '1', 'YzFGQV4gtorw', NULL, NULL, '1', NULL);
 
 -- --------------------------------------------------------
 
